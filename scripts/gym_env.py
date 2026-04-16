c31d51a7f56bb75a6af3b023e0793bf8b941ad38
Ben Ricket <ben.ricket@gmail.com>
2026-04-16 10:43:36 -0400

    Add simple training loop for getting near block

--- scripts/gym_env.py
+++ scripts/gym_env.py
@@ -2,6 +2,12 @@ import numpy as np
 from typing import Optional
 
 import gymnasium as gym
+from gymnasium.envs.registration import register
+import mujoco
+import mujoco.viewer
+from mujoco_example import ee_to_block_pos, set_joints, get_joints
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_checker import check_env
 
 class ArmEnv(gym.Env):
     """
@@ -13,43 +19,142 @@ class ArmEnv(gym.Env):
         # Holds pose of end effector, target (block to pick up) and goal (block to set the target on)
         self.observation_space = gym.spaces.Dict(
             {
-                "ee_pos": gym.spaces.Box(-1, 1, shape=(3,), dtype=float),  # x,y,z
-                "ee_rot": gym.spaces.Box(0, 2*np.pi, shape=(3,), dtype=float),  # roll,pitch,yaw
-                "target_pos": gym.spaces.Box(-1, 1, shape=(3,), dtype=float),  # x,y,z
-                "target_rot": gym.spaces.Box(0, 2*np.pi, shape=(3,), dtype=float),  # roll,pitch,yaw
-                "goal_pos": gym.spaces.Box(-1, 1, shape=(3,), dtype=float),  # x,y,z
-                "goal_rot": gym.spaces.Box(0, 2*np.pi, shape=(3,), dtype=float),  # roll,pitch,yaw
+                "ee_pos": gym.spaces.Box(-5, 5, shape=(3,), dtype=float),  # x,y,z
+                #"ee_rot": gym.spaces.Box(0, 2*np.pi, shape=(3,), dtype=float),  # roll,pitch,yaw
+                "target_pos": gym.spaces.Box(-5, 5, shape=(3,), dtype=float),  # x,y,z
+                #"target_rot": gym.spaces.Box(0, 2*np.pi, shape=(3,), dtype=float),  # roll,pitch,yaw
+                #"goal_pos": gym.spaces.Box(-1, 1, shape=(3,), dtype=float),  # x,y,z
+                #"goal_rot": gym.spaces.Box(0, 2*np.pi, shape=(3,), dtype=float),  # roll,pitch,yaw
+                "joint_pos": gym.spaces.Box(-np.pi, np.pi, shape=(7,), dtype=float)
             }
         )
 
         # We output a desired pose, which the handling of the action will approximate given dt
-        self.action_space = gym.spaces.Dict(
-            {
-                "command_pos": gym.spaces.Box(-1, 1, shape=(3,), dtype=float),  # x,y,z
-                "command_rot": gym.spaces.Box(0, 2*np.pi, shape=(3,), dtype=float),  # roll,pitch,yaw
-            }
+        self.action_space = gym.spaces.Box(
+            low=-1.0,
+            high=1.0,
+            shape=(8,),   # 7 joints + 1 gripper
+            dtype=np.float32,
         )
+
+        self.model = mujoco.MjModel.from_xml_path('../kinova_gen3/rl_scene.xml')
+        self.data = mujoco.MjData(self.model)
+        self.iter_count = 0
+
+        # attributes for reward func calculations
+        self.last_ee_to_block_dist = ee_to_block_pos(self.model,self.data)
 
     def _get_obs(self):
         # Maps environment state to the returned observation
-        pass
+        obs = {}
+        block_name = 'cube'
+        block_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_GEOM, block_name)
+        block_pos = self.data.geom_xpos[block_id].copy()
+
+        ee_name = "pinch_site"
+        ee_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, ee_name)
+        ee_pos = self.data.site_xpos[ee_id].copy()
+
+        obs["ee_pos"] = ee_pos
+        obs["target_pos"] = block_pos
+        obs["joint_pos"] = get_joints(self.model,self.data)
+        
+        return obs
 
     def _get_info(self):
         # gets auxiliary info for debugging
-        pass
+        return {}
+
+    def _compute_reward(self):
+        reward = 0.0
+
+        # reward getting closer to the block
+        dist = ee_to_block_pos(self.model,self.data)
+        dist_change = dist - self.last_ee_to_block_dist
+        reward += -10 * dist_change
+        self.last_ee_to_block_dist = dist
+
+        reward = -dist
+
+        return reward
+
+    def _is_truncated(self):
+        return False
+    
+    def _is_terminated(self):
+        dist = ee_to_block_pos(self.model,self.data)
+        if dist < 0.2:
+            return True
+        return False
     
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         """
         Starts a new episode
         """
         super().reset(seed=seed)
+        obs = self._get_obs()
+        info = self._get_info()
+        return obs,info
     
     def step(self, action):
         """
         Update step of the simulation with the given action
         """
-        pass
+        reward = 0
+        obs = 0
+        terminated = False
+        truncated = False
+        info = {}
+
+        # Apply action
+        joint_ctrl = action[0:7]
+        joint_ctrl *= np.pi # map -1,1 to -pi,pi
+        gripper_ctrl = action[7]
+        gripper_ctrl = np.interp(gripper_ctrl, [-1,1], [0,255])
+
+        curr_joint_vals = get_joints(self.model,self.data)
+        target_joint_vals = curr_joint_vals + joint_ctrl * 0.05
+        set_joints(self.model,self.data,target_joint_vals,0.0)
+
+        # Step simulation
+        mujoco.mj_step(self.model, self.data)
+
+        # Get observation
+        obs = self._get_obs()
+
+        # Get reward
+        reward = self._compute_reward()
+
+        # Check termination
+        truncated = self._is_truncated()
+        terminated = self._is_terminated()
+        if terminated:
+            reward += 10000
+
+        self.iter_count += 1
+
+        return obs, reward, terminated, truncated, info
 
 
+if __name__ == "__main__":
+
+    register(
+        id="KinovaEnv",
+        entry_point="gym_env:ArmEnv",
+        max_episode_steps=200,
+    )
+
+    env = gym.make("KinovaEnv")
+    check_env(env)
+
+    model = PPO(
+        "MultiInputPolicy",
+        env,
+        verbose=1,
+        tensorboard_log="../logs/ppo_kinova/"
+    )
+
+    model.learn(total_timesteps=25_000)
+    model.save("kinova_test")
 
 
--- scripts/mujoco_example.py
+++ scripts/mujoco_example.py
@@ -6,17 +6,13 @@ import numpy as np
 import mujoco
 import mujoco.viewer
 
-m = mujoco.MjModel.from_xml_path('../kinova_gen3/rl_scene.xml')
-d = mujoco.MjData(m)
-iter_count = 0
-
 def get_joints(m,d):
     """
     docstring
     """
     joint_names = ['joint_1','joint_2','joint_3','joint_4','joint_5','joint_6','joint_7']
     joint_vals = [d.joint(name).qpos for name in joint_names]
-    return np.array(joint_vals)
+    return np.array(joint_vals).squeeze()
 
 def set_joints(m,d,cmd_joints,gripper=0.0):
     """
@@ -25,43 +21,64 @@ def set_joints(m,d,cmd_joints,gripper=0.0):
     # gripper goes from 0 (open) to 255 (closed)
     if cmd_joints.size == 7: # we don't have control for the gripper
         cmd_joints = np.append(cmd_joints,gripper)
-        print(cmd_joints)
+        #print(cmd_joints)
     d.ctrl = cmd_joints[:]
+
+def ee_to_block_pos(m,d):
+    """
+    Returns the Euclidean distance between the end effector of the robot
+    and the block to pick up
+    """
+    block_name = 'cube'
+    block_id = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_GEOM, block_name)
+    block_pos = d.geom_xpos[block_id]
+
+    site_name = "pinch_site"
+    sid = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_SITE, site_name)
+    ee_pos = d.site_xpos[sid]   # world position (x, y, z)
     
+    dist = np.linalg.norm(block_pos - ee_pos)
+    return dist
 
-with mujoco.viewer.launch_passive(m, d) as viewer:
-  # Close the viewer automatically after 30 wall-seconds.
-  start = time.time()
-  while viewer.is_running():
-    step_start = time.time()
+if __name__ == "__main__":
+    m = mujoco.MjModel.from_xml_path('../kinova_gen3/rl_scene.xml')
+    d = mujoco.MjData(m)
+    iter_count = 0
 
-    # mj_step can be replaced with code that also evaluates
-    # a policy and applies a control signal before stepping the physics.
-    mujoco.mj_step(m, d)
+    with mujoco.viewer.launch_passive(m, d) as viewer:
+        # Close the viewer automatically after 30 wall-seconds.
+        start = time.time()
+        while viewer.is_running():
+            step_start = time.time()
 
-    if iter_count == 0:
-        geom_name = 'cube'
-        geom_id = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_GEOM, geom_name)
-        position = d.geom_xpos[geom_id]
-        #print(f"{geom_name} position: {position}")
+            # mj_step can be replaced with code that also evaluates
+            # a policy and applies a control signal before stepping the physics.
+            mujoco.mj_step(m, d)
 
-    if iter_count % 50 == 0:
-        joints = get_joints(m,d)
-        joints += 0.005
-        set_joints(m,d,joints,1.0)
-        #print(f"control: {d.ctrl}")
+            if iter_count == 0:
+                geom_name = 'cube'
+                geom_id = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_GEOM, geom_name)
+                position = d.geom_xpos[geom_id]
+                #print(f"{geom_name} position: {position}")
+
+            if iter_count % 50 == 0:
+                joints = get_joints(m,d)
+                joints += 0.005
+                set_joints(m,d,joints,1.0)
+                #print(f"control: {d.ctrl}")
+                ee_to_block_pos(m,d)
 
 
-    # Example modification of a viewer option: toggle contact points every two seconds.
-    with viewer.lock():
-      viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
+            # Example modification of a viewer option: toggle contact points every two seconds.
+            with viewer.lock():
+                viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
 
-    # Pick up changes to the physics state, apply perturbations, update options from GUI.
-    viewer.sync()
+            # Pick up changes to the physics state, apply perturbations, update options from GUI.
+            viewer.sync()
 
-    # Rudimentary time keeping, will drift relative to wall clock.
-    time_until_next_step = m.opt.timestep - (time.time() - step_start)
-    if time_until_next_step > 0:
-      time.sleep(time_until_next_step)
-    
-    iter_count = (iter_count + 1) % 500
+            # Rudimentary time keeping, will drift relative to wall clock.
+            time_until_next_step = m.opt.timestep - (time.time() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
+            
+            iter_count = (iter_count + 1) % 500
